891a81bebc2490bfb2574347cad513c42f0ffbb7
Shayla <shayladwhite1@gmail.com>
2023-10-25 00:27:31 -0700

    imported user0reg to app-routing.module.ts

--- soulbloom-frontend/src/app/app-routing.module.ts
+++ soulbloom-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'user-registration', component: UserRegistrationComponent },
+  // Other routes...
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
