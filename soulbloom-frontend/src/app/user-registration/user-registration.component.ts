fe7d95e1a8500f6a48e5d34cc71707cf7d41e943
Shayla <shayladwhite1@gmail.com>
2023-10-25 00:32:52 -0700

    updated the user-re comp to reflect the params needed for acceptance of data

--- soulbloom-frontend/src/app/user-registration/user-registration.component.ts
+++ soulbloom-frontend/src/app/user-registration/user-registration.component.ts
@@ -7,18 +7,24 @@ import { ApiService } from '../api.service';
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.css']
 })
-export class UserRegistrationComponent implements OnInit {
-  data: any; // Use the 'any' type for data
+export class UserRegistrationComponent {
+  constructor(private apiService: ApiService) {}
 
-  constructor(private apiService: ApiService) { }
+  registerUser() {
+    const userData = {
+      username: 'johndoe',
+      emailAddress: 'jonh@aol.com',
+      password: 'password',
+    };
 
-  ngOnInit() {
-    this.apiService.getDataFromBackend().subscribe(
-      (data) => {
-        this.data = data;
+    this.apiService.registerUser(userData).subscribe(
+      (response) => {
+        // Handle successful registration
+        console.log('Registration successful:', response);
       },
       (error) => {
-        console.error('Error:', error);
+        // Handle registration errors
+        console.error('Registration error:', error);
       }
     );
   }
