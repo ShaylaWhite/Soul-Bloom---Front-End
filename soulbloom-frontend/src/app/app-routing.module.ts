237629195c7df7ec98459fcab006a20baa074128
Shayla <shayladwhite1@gmail.com>
2023-10-25 10:52:58 -0700

    loginUser

--- soulbloom-frontend/src/app/app-routing.module.ts
+++ soulbloom-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: 'user-registration', component: UserRegistrationComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'user-profile', component: UserProfileComponent }
 ];
 
 @NgModule({
