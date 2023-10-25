0c1b462a9ac7d5658a342a70a8e79d850c182854
Shayla <shayladwhite1@gmail.com>
2023-10-25 10:59:00 -0700

    added user profile loigc to render from successful login and created auth guard toprotet user personal information

--- soulbloom-frontend/src/app/app-routing.module.ts
+++ soulbloom-frontend/src/app/app-routing.module.ts
@@ -4,14 +4,27 @@ import { UserRegistrationComponent } from './user-registration/user-registration
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
+// Import CanActivate and Router from '@angular/router'
+import { CanActivate, Router } from '@angular/router';
+
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    // Implement your authentication logic here.
+    // For demonstration purposes, always return true (allow access).
+    return true;
+  }
+}
+
 const routes: Routes = [
   { path: 'user-registration', component: UserRegistrationComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'user-profile', component: UserProfileComponent }
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- soulbloom-frontend/src/app/login/login.component.ts
+++ soulbloom-frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router'; // Import the Router
 
 @Component({
   selector: 'app-login',
@@ -12,24 +13,25 @@ export class LoginComponent {
 
   constructor(private apiService: ApiService, private router: Router) {}
 
-loginUser() {
-  const userData = this.user; // Use the user object to send data
-  this.apiService.loginUser(userData).subscribe(
-    (response: any) => {
-      // Handle successful login
-      console.log('Login successful:', response);
+  loginUser() {
+    const userData = this.user; // Use the user object to send data
+    this.apiService.loginUser(userData).subscribe(
+      (response: any) => {
+        // Handle successful login
+        console.log('Login successful:', response);
 
-      // Redirect to user profile upon successful login
-      this.router.navigate(['/user-profile']);
-    },
-    (error: any) => {
-      // Handle login errors
-      console.error('Login error:', error);
+        // Redirect to user profile upon successful login
+        this.router.navigate(['/user-profile']);
+      },
+      (error: any) => {
+        // Handle login errors
+        console.error('Login error:', error);
 
-      if (error.status === 401) {
-        // Display an error message to the user
-        this.loginError = 'Authentication failed. Please check your credentials.';
+        if (error.status === 401) {
+          // Display an error message to the user
+          this.loginError = 'Authentication failed. Please check your credentials.';
+        }
       }
-    }
-  );
+    );
+  }
 }
