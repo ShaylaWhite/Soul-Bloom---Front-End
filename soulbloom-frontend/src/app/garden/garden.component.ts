db579a3d9b47fda64544c4fcac286d943efcf6dd
Shayla <shayladwhite1@gmail.com>
2023-10-26 04:19:22 -0700

    added garden to the user-profile compoent

--- soulbloom-frontend/src/app/garden/garden.component.ts
+++ soulbloom-frontend/src/app/garden/garden.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-garden',
   templateUrl: './garden.component.html',
   styleUrls: ['./garden.component.css']
 })
-export class GardenComponent {
+export class GardenComponent implements OnInit {
+  gardenName: string = '';
+  gardenId: number = 1; // Replace with the appropriate garden ID
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Load garden data when the component initializes
+    this.loadGardenData();
+  }
+
+  loadGardenData() {
+    this.http
+      .get('http://localhost:9092/api/users/gardens/' + this.gardenId)
+      .subscribe((gardenData: any) => {
+        // Handle garden data
+        this.gardenName = gardenData.name;
+      });
+  }
+
+  waterGarden() {
+    this.http
+      .put('http://localhost:9092/api/users/water-garden/' + this.gardenId, {})
+      .subscribe((response: any) => {
+        // Handle the response or perform additional actions
+        console.log('Garden watered:', response);
+      });
+  }
 }
--- soulbloom-frontend/src/app/user-profile/user-profile.component.ts
+++ soulbloom-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+  showGarden = false;
+  gardenId: number | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  showGardenComponent() {
+    // Create a garden
+    this.http.post('http://localhost:9092/api/users/create-garden', { name: 'My Beautiful Garden' })
+      .subscribe((response: any) => {
+        // Handle the response or perform additional actions
+        console.log('Garden created:', response);
+        this.gardenId = response.id;
+
+        // Load the garden data
+        this.http.get('http://localhost:9092/api/users/gardens/' + this.gardenId)
+          .subscribe((gardenData: any) => {
+            // Handle garden data and render the garden component
+            console.log('Loaded garden data:', gardenData);
+            this.showGarden = true;
+          });
+      });
+  }
+
+  waterGarden() {
+    this.http.put('http://localhost:9092/api/users/water-garden/' + this.gardenId, {})
+      .subscribe((response: any) => {
+        // Handle the response or perform additional actions
+        console.log('Garden watered:', response);
+      });
+  }
 }
