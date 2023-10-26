d0190cc92ac639c79e3e69d0dc88d6c92de2b631
Shayla <shayladwhite1@gmail.com>
2023-10-26 12:15:05 -0700

    updated userReg method and html files for styling

--- soulbloom-frontend/src/app/user-registration/user-registration.component.ts
+++ soulbloom-frontend/src/app/user-registration/user-registration.component.ts
@@ -7,58 +7,60 @@ import { Router } from '@angular/router';
   templateUrl: './user-registration.component.html',
   styleUrls: ['./user-registration.component.css']
 })
+
+
 export class UserRegistrationComponent {
   user: any = {
     username: '',
-    email: '',
+    emailAddress: '',
     password: ''
   };
   registrationError: string | undefined;
+  userData: any; // Declare userData as a class property
 
   constructor(private apiService: ApiService, private router: Router) {}
 
   registerUser() {
     console.log('Register user function called.'); // Add this line
-    const userData = this.user; // Capture the user data
-    console.log('User Data:', userData); // Log the user data
-
-    
+    this.userData = this.user; // Capture the user data
 
     if (!this.user.password) {
       // Password is empty, show an error or prevent form submission
       this.registrationError = 'Password cannot be empty';
     } else {
       // Password is not empty, proceed with registration
-      const userData = this.user;
-      this.apiService.registerUser(userData).subscribe(
+      this.apiService.registerUser(this.userData).subscribe(
         (response: any) => {
           // Handle successful registration
-          console.log('Registration successful:', response);
-  // Navigate to the user profile component
-  this.router.navigate(['/user-profile']);
+          this.handleRegistrationSuccess(response);
         },
         (error: any) => {
           // Handle registration errors
           this.handleRegistrationError(error);
-          return false;
         }
       );
     }
   }
-  
-  
 
   private handleRegistrationSuccess(response: any) {
     console.log('Registration successful:', response);
-    // Navigate to the user profile component
-    this.router.navigate(['/user-profile']);
+    if (response && response.id) {
+      const userId = response.id; // Use the actual user ID from the response
+      this.router.navigate(['/user-profile', userId]);
+    } else {
+      console.error('Invalid user ID received.');
+    }
   }
+  
 
   private handleRegistrationError(error: any) {
     console.error('Registration error:', error);
-
+  
     if (error.status === 401) {
       this.registrationError = 'Registration failed. Please check your data.';
+    } else {
+      // Show an alert to the user
+      alert('Registration failed. Please check your data ');
     }
   }
 }
